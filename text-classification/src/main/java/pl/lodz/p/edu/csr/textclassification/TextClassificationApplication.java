16645b9f4a610a738f5479d157741101d9ecc424
Grala96 <gracjan.grala.tel@gmail.com>
2020-03-08 23:16:27 +0100

    Init project

---
+++ text-classification/src/main/java/pl/lodz/p/edu/csr/textclassification/TextClassificationApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.edu.csr.textclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextClassificationApplication.class, args);
+	}
+
+}
---
+++ text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/TextClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.edu.csr.textclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
