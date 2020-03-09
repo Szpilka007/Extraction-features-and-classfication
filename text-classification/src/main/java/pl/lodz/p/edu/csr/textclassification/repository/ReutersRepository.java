778282a7187806951570a14dbaeee9531c1af5be
Grala96 <gracjan.grala.tel@gmail.com>
2020-03-09 03:24:51 +0100

    Added basic structure for reuters

---
+++ text-classification/src/main/java/pl/lodz/p/edu/csr/textclassification/repository/ReutersRepository.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.edu.csr.textclassification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReutersRepository extends JpaRepository<Reuters, Long> {
+
+
+
+}
