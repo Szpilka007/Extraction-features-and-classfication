a6edc99c134ffcf1f46f1ecf6df4d60794d0035c
Adam <adam.krzanowski1998@gmail.com>
2020-04-05 11:10:05 +0200

    Dodanie miary

--- text-classification/src/main/java/pl/lodz/p/edu/csr/textclassification/service/metrics/CosineAmplitude.java
+++ text-classification/src/main/java/pl/lodz/p/edu/csr/textclassification/service/metrics/CosineAmplitude.java
@@ -2,16 +2,33 @@ package pl.lodz.p.edu.csr.textclassification.service.metrics;
 
 import pl.lodz.p.edu.csr.textclassification.repository.entities.FeatureEntity;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 public class CosineAmplitude implements Metric {
     @Override
     public Double calculate(Vector<FeatureEntity> a, Vector<FeatureEntity> b) throws Exception {
-        return null;
+        validatePoints(a,b);
+
+        Iterator aIterator = a.iterator();
+        Iterator bIterator = b.iterator();
+
+        double counter =0.0, dimASum =0.0, dimBSum =0.0;
+        FeatureEntity dimA, dimB;
+
+        while(aIterator.hasNext()){
+            dimA = (FeatureEntity) aIterator.next();
+            dimB = (FeatureEntity) bIterator.next();
+            counter += dimA.getValue() * dimB.getValue();
+            dimASum += dimA.getValue()*dimA.getValue();
+            dimBSum += dimB.getValue()*dimB.getValue();
+        }
+
+        return Math.abs(counter) / Math.sqrt(dimASum*dimBSum);
     }
 
     @Override
     public MetricType getMetricsType() {
-        return null;
+        return MetricType.COSINE_AMPLITUDE;
     }
 }
--- text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/metrics/CosineAmplitudeTest.java
+++ text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/metrics/CosineAmplitudeTest.java
@@ -1,12 +1,28 @@
 package pl.lodz.p.edu.csr.textclassification.service.metrics;
 
 import org.junit.jupiter.api.Test;
+import pl.lodz.p.edu.csr.textclassification.repository.entities.FeatureEntity;
+
+import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CosineAmplitudeTest {
 
     @Test
-    void calculate() {
+    void calculate() throws Exception {
+        Vector pointA = new Vector();
+        pointA.add(FeatureEntity.builder().value(5.0).build());
+        pointA.add(FeatureEntity.builder().value(7.0).build());
+
+        Vector pointB = new Vector();
+        pointB.add(FeatureEntity.builder().value(2.0).build());
+        pointB.add(FeatureEntity.builder().value(2.0).build());
+
+        Metric metrics = new CosineAmplitude();
+
+        Double result = metrics.calculate(pointA, pointB);
+
+        assertEquals(24/Math.sqrt(592), result, 0.2);
     }
 }
