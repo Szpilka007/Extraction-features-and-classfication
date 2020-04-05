bd47c8365786c7889bafb4f82456e107462acd8d
Grala96 <gracjan.grala.tel@gmail.com>
2020-04-05 09:51:57 +0200

    Added empty metric jaccard-index

---
+++ text-classification/src/main/java/pl/lodz/p/edu/csr/textclassification/service/metrics/JaccardIndex.java
@@ -0,0 +1,17 @@
+package pl.lodz.p.edu.csr.textclassification.service.metrics;
+
+import pl.lodz.p.edu.csr.textclassification.repository.entities.FeatureEntity;
+
+import java.util.Vector;
+
+public class JaccardIndex implements Metric {
+    @Override
+    public Double calculate(Vector<FeatureEntity> a, Vector<FeatureEntity> b) throws Exception {
+        return null;
+    }
+
+    @Override
+    public MetricType getMetricsType() {
+        return null;
+    }
+}
---
+++ text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/metrics/JaccardIndexTest.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.edu.csr.textclassification.service.metrics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JaccardIndexTest {
+
+    @Test
+    void calculate() {
+    }
+}
