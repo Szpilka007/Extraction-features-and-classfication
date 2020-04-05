b81ea9ad8c171746eb11ae7db400dc64b12bdf48
Adam <adam.krzanowski1998@gmail.com>
2020-04-05 12:34:41 +0200

    Dodanie JaccardIndex

--- text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/metrics/JaccardIndexTest.java
+++ text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/metrics/JaccardIndexTest.java
@@ -1,12 +1,28 @@
 package pl.lodz.p.edu.csr.textclassification.service.metrics;
 
 import org.junit.jupiter.api.Test;
+import pl.lodz.p.edu.csr.textclassification.repository.entities.FeatureEntity;
+
+import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JaccardIndexTest {
 
     @Test
-    void calculate() {
+    void calculate() throws Exception {
+        Vector pointA = new Vector();
+        pointA.add(FeatureEntity.builder().value(2.0).build());
+        pointA.add(FeatureEntity.builder().value(2.0).build());
+
+        Vector pointB = new Vector();
+        pointB.add(FeatureEntity.builder().value(2.0).build());
+        pointB.add(FeatureEntity.builder().value(4.0).build());
+
+        Metric metrics = new JaccardIndex();
+
+        Double result = metrics.calculate(pointA, pointB);
+
+        assertEquals(0.5, result);
     }
 }
