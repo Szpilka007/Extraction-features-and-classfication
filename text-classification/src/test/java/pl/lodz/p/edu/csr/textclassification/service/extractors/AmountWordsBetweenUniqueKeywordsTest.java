0364149c7c2b2b524fb5d523ae6aa0a570bed369
Adam <adam.krzanowski1998@gmail.com>
2020-04-07 12:08:57 +0200

    Poprawka

--- text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/extractors/AmountWordsBetweenUniqueKeywordsTest.java
+++ text-classification/src/test/java/pl/lodz/p/edu/csr/textclassification/service/extractors/AmountWordsBetweenUniqueKeywordsTest.java
@@ -11,14 +11,14 @@ public class AmountWordsBetweenUniqueKeywordsTest {
 
     Extractor aosckdp = new AmountWordsBetweenUniqueKeywords(new TextProcessor());
 
-    String text = "Sun dog cat cat sun moon. Test dance dance. Been. dance flower";
+    String text = "Black sun dog cat cat sun moon. Test dance dance. Been. dance flower";
 
     @Test
     public void extract(){
         ReutersEntity reutersEntity = ReutersEntity.builder().body(text).build();
         Double actual = aosckdp.extract(reutersEntity);
-        // 4 unique words - > dog, moon, Test, flower
-        double expected = (double) (4 + 1 + 4) / 3;
+        // 4 unique words - > Black, dog, moon, Test, flower
+        double expected = (double) (2+ 4 + 1 + 4) / 4;
 
         Assert.assertEquals(expected, actual, 0.1);
     }
